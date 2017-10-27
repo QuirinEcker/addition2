7344d3ed71bd863ece50c4de59bc644ca176363e
QuirinEcker <quirin.ecker@bajupa.com>
2017-10-27 18:32:59 +0200

    complete code

---
+++ src/main/java/Ecker_Addition2.java
@@ -0,0 +1,14 @@
+public class Ecker_Addition2 {
+
+    public static void main(String[] args) {
+
+        final int X = 5;
+        final int Y = -7;
+        final int ERGEBNIS = X + Y;
+
+        System.out.printf("%2d \n" , X);
+        System.out.println(Y);
+        System.out.println("--");
+        System.out.println(ERGEBNIS);
+    }
+}
